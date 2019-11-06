d9878d6b3cfe9cd074a701579c5140409385971d
dwlam8790 <49571438+dwlam8790@users.noreply.github.com>
2019-11-06 02:55:31 -0500

    added sprites and adjusted background for 2D

--- js/game.js
+++ js/game.js
@@ -1,6 +1,23 @@
 //create a new scene
-let gameScene = new Phaser.Scene('Game');
+//init()>preload()>create()>update()
+const gameScene = new Phaser.Scene('Game');
 
+
+//load assets
+gameScene.preload = function(){
+    //loading images
+    this.load.image('background', 'assets/background.png');
+    this.load.image('player', 'assets/player.png');
+
+};
+//call after preload ends
+gameScene.create = function() {
+    //create background sprite
+    //2D cordinate system and needs to be sized accordingly
+   let bg = this.add.sprite(0, 0, 'background');
+   //changes origin to top-left corner
+   bg.setOrigin(0,0); 
+};
 
 //set the config of the game
 let config = {
